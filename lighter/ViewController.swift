49523e56c7a524d5200b56a50fe5a90f6bfdcf3e
Marat <kosjachok8@yandex.ru>
2022-06-03 15:26:12 +0300

    ibOutlet

--- lighter/ViewController.swift
+++ lighter/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    
+    @IBOutlet var yellowLight: UIView!
+    
+    @IBOutlet var greenLight: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func switchingFlash() {
+    }
+    
 }
 
